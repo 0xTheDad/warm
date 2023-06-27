a56f1d25f02fc6312cedb61baf9681d1e780eec8
0xTheDad <ekitaeff@yandex.ru>
2023-06-27 12:59:00 +0300

    Add tests

--- src/bugfix_20251202.py
+++ src/bugfix_20251202.py
@@ -255,3 +255,8 @@ def historical_feature():
     """Feature added on 2025-02-03 09:30:00"""
     print('Historical feature working')
     return True
+# Historical update 2023-06-27 12:59:00
+def historical_feature():
+    """Feature added on 2023-06-27 12:59:00"""
+    print('Historical feature working')
+    return True
