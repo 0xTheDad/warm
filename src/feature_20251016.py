5111d1031ab3fc8e1dfba2c854921913b9366369
0xTheDad <ekitaeff@yandex.ru>
2023-11-15 18:48:00 +0300

    Update README

--- src/feature_20251016.py
+++ src/feature_20251016.py
@@ -309,3 +309,8 @@ def historical_feature():
     """Feature added on 2023-06-01 11:28:00"""
     print('Historical feature working')
     return True
+# Historical update 2023-11-15 18:48:00
+def historical_feature():
+    """Feature added on 2023-11-15 18:48:00"""
+    print('Historical feature working')
+    return True
